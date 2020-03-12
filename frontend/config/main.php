9e63dddf1c063a10b2ca94d66a3a38a09bf227d1
egorrishe <egorrishe@yandex.ru>
2020-03-12 14:01:41 +0200

    init: config

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -36,14 +36,15 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
+        'assetManager' => [
+            'linkAssets' => true,
+        ],
     ],
     'params' => $params,
 ];
